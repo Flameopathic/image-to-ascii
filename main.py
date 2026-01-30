e5740e1f97bf983187931ff7d039540d321266c9
Flameopathic <flameopathic@gmail.com>
2026-01-30 11:07:37 -0600

    refactor

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from statistics import fmean
 
 from PIL import Image, ImageChops, ImageDraw, ImageFont, ImageStat
 
-glyph_ratio = 2  # x / y
 
-
-def divide_image(im: Image.Image, x_segs: int):
+def divide_image(im: Image.Image, x_segs: int, glyph_ratio: float):
     x_step = im.width / x_segs
     y_step = x_step * glyph_ratio
     y_segs = ceil(im.height / y_step)
+    mode = im.mode
 
     # make image tall enough
     im = im.convert("RGBA")
     new_im = Image.new("RGBA", (im.width, int(y_step * y_segs)), "WHITE")
     new_im.paste(im, (0, 0), im)
-    im = new_im.convert("L")
+    im = new_im.convert(mode)
 
     segments = []
     for yi in range(y_segs):
@@ -36,52 +35,59 @@ def divide_image(im: Image.Image, x_segs: int):
     return segments
 
 
+def gen_char_images(chars, font):
+    width = max([int(font.getbbox(char)[2]) for char in chars])
+    height = max([int(font.getbbox(char)[3]) for char in chars])
+    char_image_dict = {}
+    for char in chars:
+        im = Image.new("L", (width, height), 255)
+        draw = ImageDraw.Draw(im)
+        draw.text((0, 0), char, 0, font=font)
+
+        char_image_dict[char] = im
+
+    return char_image_dict
+
+
+def get_brightness(im: Image.Image):
+    return fmean(ImageStat.Stat(im.convert("L")).mean) / 255
+
+
+def normalize_brightnesses(char_brightness_dict: dict[str, float]):
+    char_brightness_dict = dict(
+        sorted(char_brightness_dict.items(), key=lambda item: item[1])
+    )
+    min_value = list(char_brightness_dict.values())[0]
+    char_brightness_dict = {
+        char: char_brightness_dict[char] - min_value for char in char_brightness_dict
+    }
+    max_value = list(char_brightness_dict.values())[-1]
+    char_brightness_dict = {
+        char: char_brightness_dict[char] / max_value for char in char_brightness_dict
+    }
+    return char_brightness_dict
+
+
 def map2d(func, grid):
     return [[func(value) for value in row] for row in grid]
 
 
-def get_char_brightness_dict(chars: str, font_path: str):
-    font = ImageFont.truetype(font=font_path, size=50)
-    char_dict: dict[str, float] = {}
+def get_char_brightness_dict(char_image_dict):
+    char_brightness_dict = normalize_brightnesses(
+        {char: get_brightness(char_image_dict[char]) for char in char_image_dict}
+    )
 
-    for char in chars:
-        box = font.getbbox(char)
-        width = int(box[2])
-        height = int(box[3])
-
-        im = Image.new("L", (width, height), 0)
-        draw = ImageDraw.Draw(im)
-        draw.text((0, 0), char, 255, font=font)
-        colors = im.getcolors()
-
-        total = 0
-        if type(colors) is list:
-            for c in colors:
-                if type(c[0]) is int and type(c[1]) is int:
-                    total += c[0] * c[1]
-
-        char_dict[char] = total
-
-    max = 0
-    for char in char_dict:
-        if char_dict[char] > max:
-            max = char_dict[char]
-
-    if max != 0:
-        for char in char_dict:
-            char_dict[char] = 1 - (char_dict[char] / max)
-
-    return dict(sorted(char_dict.items(), key=lambda item: item[1]))
+    return char_brightness_dict
 
 
-def brightness_converter(segments: list[list[Image.Image]], char_dict):
+def brightness_converter(segments: list[list[Image.Image]], char_brightness_dict):
     brightness_grid = map2d(lambda im: fmean(ImageStat.Stat(im).mean) / 255, segments)
 
     def round_to_char(seg_brightness: float):
-        return list(char_dict.keys())[
-            list(char_dict.values()).index(
+        return list(char_brightness_dict.keys())[
+            list(char_brightness_dict.values()).index(
                 min(
-                    char_dict.values(),
+                    char_brightness_dict.values(),
                     key=lambda char_brightness: abs(char_brightness - seg_brightness),
                 )
             )
@@ -91,28 +97,13 @@ def brightness_converter(segments: list[list[Image.Image]], char_dict):
     return "\n".join(rows)
 
 
-def px_diff_converter(segments: list[list[Image.Image]], chars: str, font_path: str):
-    font = ImageFont.truetype(font=font_path, size=50)
-
-    width = max([int(font.getbbox(char)[2]) for char in chars])
-    height = max([int(font.getbbox(char)[3]) for char in chars])
-
-    char_dict = {}
-    for char in chars:
-        im = Image.new("L", (width, height), 255)
-        draw = ImageDraw.Draw(im)
-        draw.text((0, 0), char, 0, font=font)
-
-        im = im.resize(segments[0][0].size)
-
-        char_dict[char] = im
-
+def px_diff_converter(segments: list[list[Image.Image]], char_image_dict):
     def compare_to_char(seg: Image.Image):
         best_char = ""
         best_diff = -1
-        for char in char_dict:
+        for char in char_image_dict:
             diff = fmean(
-                ImageStat.Stat(ImageChops.difference(seg, char_dict[char])).mean
+                ImageStat.Stat(ImageChops.difference(seg, char_image_dict[char])).mean
             )
             if best_diff == -1 or diff < best_diff:
                 best_diff = diff
@@ -126,6 +117,7 @@ def px_diff_converter(segments: list[list[Image.Image]], chars: str, font_path: 
 if __name__ == "__main__":
     chars = " `1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
     font_path = "./GeistMono-Regular.otf"
+    font = ImageFont.truetype(font=font_path, size=50)
     img_path = "./img.png"
 
     with Image.open(img_path).convert("RGBA") as im:
@@ -134,9 +126,23 @@ if __name__ == "__main__":
         new_im.paste(im, (0, 0), im)
         im = new_im.convert("L")  # greyscale
 
-        segments = divide_image(im, 200)
+        char_image_dict: dict[str, Image.Image] = gen_char_images(chars, font)
 
-        # char_brightness_dict = get_char_brightness_dict(chars, font_path)
+        segments = divide_image(
+            im,
+            200,
+            list(char_image_dict.values())[0].height
+            / list(char_image_dict.values())[0].width,
+        )
+
+        char_image_dict = {
+            char: char_image_dict[char].resize(segments[0][0].size)
+            for char in char_image_dict
+        }
+
+        # char_brightness_dict = get_char_brightness_dict(char_image_dict)
+
+        # print(char_brightness_dict)
         # print(brightness_converter(segments, char_brightness_dict))
 
-        print(px_diff_converter(segments, chars, font_path))
+        print(px_diff_converter(segments, char_image_dict))
